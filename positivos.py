d81dcab07c51369460e8a9842db4b4674ec09a2a
jodaruve <jose.rueda.2019@upb.edu.co>
2019-03-01 11:21:45 -0500

    ejercicio dos resuelto 2.0

--- positivos.py
+++ positivos.py
@@ -1,5 +1,9 @@
+## pares
+t_pares=0
 a=int(input("por favor ingrese un  numero para saber si es par o impar "))
-if a%2==0:
-	print("el numero es par")
-else:
-	print ("el numero es impar")
+while a%2==0:
+    print("el numero es par")
+    a=int(input("por favor ingrese un  numero para saber si es par o impar "))
+    t_pares=t_pares+1
+
+print("el total de pares fue de:", t_pares)
